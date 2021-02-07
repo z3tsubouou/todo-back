a5e7f2aed7eebd876fc6026c20c46051628354a3
Davgatseren <davgatseren@gmail.com>
2021-02-07 14:17:53 +0800

    origin nemsen

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv/config");
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://fronttodolistapp.herokuapp.com" }));
 app.use(express.json());
 
 //routes
